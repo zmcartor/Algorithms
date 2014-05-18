67ce6bb0fc2ee608e8cd9075a2ef7e593c2b052f
Zach <zmcartor@gmail.com>
2014-05-17 22:07:22 -0600

    reporposed mergesort inversion count

--- Python/mergesort_inversions.py
+++ Python/mergesort_inversions.py
@@ -1,22 +1,18 @@
-# not pround of the leaking global variable thing :/
-inversions = 0
-def mergesort_inversions(list):
+def mergesort_inversions(list, inversions =0):
 	
-	def combine(list1, list2):
+	def combine(list1, list2, inversions =0):
 		combined = []
 		while list1 and list2:
 			if list1[0] <= list2[0]:
 				combined.append(list1.pop(0))
 			else:
 				combined.append(list2.pop(0))
-				#inversions equal length of list1 here.
-				global inversions
 				inversions += len(list1)
-		return combined + list1 + list2
+		return (combined + list1 + list2, inversions)
 	
 	if len(list) <= 1:
-		return list
+		return (list, inversions)
 
 	list1 = mergesort_inversions(list[0:len(list)/2])
 	list2 = mergesort_inversions(list[len(list)/2:len(list)])
-	return combine(list1, list2)
+	return combine(list1[0], list2[0], list1[1]+list2[1])
